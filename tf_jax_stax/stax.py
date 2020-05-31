f7db9b1c7367db8df5b0dc6d170cb5f5dd1f5c7f
DarrenZhang01 <18633059886@163.com>
2020-05-31 15:16:06 -0400

    Finish the draft version for Dense layer using TensorFlow equivalent APIs.

--- tf_jax_stax/stax.py
+++ tf_jax_stax/stax.py
@@ -16,68 +16,83 @@ from tensorflow.nn import (relu, log_softmax, softmax, softplus, sigmoid, elu,
                            leaky_relu, selu, normalize)
 # TODO: tf.nn.gelu
 
-def Dense():
+from tensorflow.keras.initializers import GlorotNormal, RandomNormal, Ones, Zeros
 
-    pass
+def Dense(out_dim, W_init=None, bias_init=None):
+  """
+  Dense/Fully-connected layer.
+  """
+  def init_func(rng_seed, input_shape):
+    """ rng_seed is for specifying seed for the random initializers. """
+    output_shape = input_shape[:-1] + (out_dim,)
+    W_init = GlorotNormal(rng_seed) if not W_init else W_init
+    bias_init = RandomNormal(seed=rng_seed) if not bias_init else bias_init
+    W, b = W_init((input_shape[-1], out_dim)), bias_init((out_dim,))
+    return output_shape, (W, b)
+  def apply_func(params, inputs, **kwargs):
+    W, b = params
+    return math_ops.dot(inputs, W) + b
+  return init_func, apply_func
 
 def GeneralConv():
 
-    pass
+
+  pass
 
 def GeneralConvTranpose():
 
-    pass
+  pass
 
 def BatchNorm():
 
-    pass
+  pass
 
 def elementwise():
 
-    pass
+  pass
 
 def _pooling_layer():
 
-    pass
+  pass
 
 def _normalize_by_window_size():
 
-    pass
+  pass
 
 def Flatten():
 
-    pass
+  pass
 Flatten = Flatten()
 
 def Identity():
 
-    pass
+  pass
 Identity = Identity()
 
 def FanOut():
 
-    pass
+  pass
 
 def FanInSum():
 
-    pass
+  pass
 FanInSum = FanInSum()
 
 def FanInConcat():
 
-    pass
+  pass
 
 def Dropout():
 
-    pass
+  pass
 
 def serial():
 
-    pass
+  pass
 
 def parallel():
 
-    pass
+  pass
 
 def shape_dependent():
 

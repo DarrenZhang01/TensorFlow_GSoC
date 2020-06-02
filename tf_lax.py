f5e822c4192ad65f46977c5a5660b844cde2e8c6
DarrenZhang01 <18633059886@163.com>
2020-06-02 19:44:47 -0400

    Update the helper functions needed and the main APIs.

--- tf_lax.py
+++ tf_lax.py
@@ -14,7 +14,7 @@ This file contains TF equivalences for:
 from tensorflow.compiler.xla.python import xla_client
 import numpy as onp
 
-
+#-------------------------------helper functions------------------------------#
 def _ceil_divide(x1, x2):
   return -onp.floor_divide(onp.negative(x1), x2)
 
@@ -40,10 +40,10 @@ def padtype_to_pads(in_shape, window_shape, window_strides, padding):
   else:
     msg = "Unknown padding type: {}."
     raise TypeError(msg.format(padding))
-
+  
 
 def _conv_transpose_padding(k, s, padding):
-  
+
   if padding == 'SAME':
     pad_len = k + s - 2
     if s > k - 1:
@@ -57,6 +57,24 @@ def _conv_transpose_padding(k, s, padding):
     raise ValueError('Padding mode must be `SAME` or `VALID`.')
   pad_b = pad_len - pad_a
   return pad_a, pad_b
+
+
+def conv_shape_tuple(lhs_shape, rhs_shape, strides, pads, batch_group_count=1):
+  """Compute the shape tuple of a conv given input shapes in canonical order."""
+  if isinstance(pads, str):
+    pads = padtype_to_pads(lhs_shape[2:], rhs_shape[2:], strides, pads)
+  if len(pads) != len(lhs_shape) - 2:
+    msg = "Wrong number of explicit pads for convolution: expected {}, got {}."
+    raise TypeError(msg.format(len(lhs_shape) - 2, len(pads)))
+
+  lhs_padded = onp.add(lhs_shape[2:], onp.sum(onp.array(pads).reshape(-1, 2),
+                                              axis=1))
+  out_space = onp.floor_divide(
+    onp.subtract(lhs_padded, rhs_shape[2:]), strides) + 1
+  out_space = onp.maximum(0, out_space)
+  assert lhs_shape[0] % batch_group_count == 0
+  out_shape = (lhs_shape[0] // batch_group_count, rhs_shape[0])
+  return tuple(out_shape + tuple(out_space))
 
 
 def conv_general_permutations(dimension_numbers):
@@ -79,6 +97,10 @@ def conv_general_permutations(dimension_numbers):
     raise TypeError(msg.format(dimension_numbers))
 
 
+#---------------------------------main APIs------------------------------------#
+
+# helper function: 1. conv_general_permutations
+#                  2. conv_shape_tuple
 def conv_general_shape_tuple(lhs_shape, rhs_shape, window_strides, padding,
                              dimension_numbers):
   lhs_perm, rhs_perm, out_perm = conv_general_permutations(dimension_numbers)
@@ -87,14 +109,15 @@ def conv_general_shape_tuple(lhs_shape, rhs_shape, window_strides, padding,
   out_trans = conv_shape_tuple(lhs_trans, rhs_trans, window_strides, padding)
   return tuple(onp.take(out_trans, onp.argsort(out_perm)))
 
-
+# helper function: 1. conv_general_permutations
+#                  2. _conv_transpose_padding
 def conv_transpose_shape_tuple(lhs_shape, rhs_shape, window_strides, padding,
                                dimension_numbers):
   lhs_perm, rhs_perm, out_perm = conv_general_permutations(dimension_numbers)
   lhs_trans = onp.take(lhs_shape, lhs_perm)
   rhs_trans = onp.take(rhs_shape, rhs_perm)
   if isinstance(padding, str):
-    padding = [_conv_transpose_padding(k, s, padding)
+    padding = _conv_transpose_padding(k, s, padding)
                for k,s in zip(rhs_trans[2:], window_strides)]
   padding = list(map(onp.sum, padding))
   unpad_out_space = [(i-1) * s - k + 2
@@ -105,7 +128,7 @@ def conv_transpose_shape_tuple(lhs_shape, rhs_shape, window_strides, padding,
   out_trans = tuple((lhs_trans[0], rhs_trans[0]) + tuple(out_space))
   return tuple(onp.take(out_trans, onp.argsort(out_perm)))
 
-
+# helper function: 1. padtype_to_pads
 def reduce_window_shape_tuple(operand_shape, window_dimensions, window_strides,
                               padding):
   pads = padtype_to_pads(operand_shape, window_dimensions, window_strides, padding)
@@ -113,24 +136,3 @@ def reduce_window_shape_tuple(operand_shape, window_dimensions, window_strides,
   t = onp.floor_divide(
       onp.subtract(operand_padded, window_dimensions), window_strides) + 1
   return tuple(t)
-
-_reduce_window_max_translation_rule = partial(
-    _reduce_window_chooser_translation_rule, max_p, _get_max_identity)
-reduce_window_max_p = standard_primitive(
-    _common_reduce_window_shape_rule, _input_dtype, 'reduce_window_max',
-    _reduce_window_max_translation_rule)
-ad.defjvp(reduce_window_max_p, partial(_reduce_window_chooser_jvp_rule, max_p))
-batching.primitive_batchers[reduce_window_max_p] = partial(
-  _reduce_window_batch_rule, _reduce_window_max)
-
-_reduce_window_min_translation_rule = partial(
-    _reduce_window_chooser_translation_rule, min_p, _get_min_identity)
-reduce_window_min_p = standard_primitive(
-    _common_reduce_window_shape_rule, _input_dtype, 'reduce_window_min',
-    _reduce_window_min_translation_rule)
-ad.defjvp(reduce_window_min_p, partial(_reduce_window_chooser_jvp_rule, min_p))
-
-_reduce_window_min_batch_rule = partial(_reduce_window_batch_rule,
-                                        _reduce_window_min)
-batching.primitive_batchers[reduce_window_min_p] = partial(
-  _reduce_window_batch_rule, _reduce_window_min)

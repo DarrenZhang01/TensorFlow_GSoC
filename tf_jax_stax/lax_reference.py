eb4e12312cf46b25c1b9fb723c80b47d472b4fd8
DarrenZhang01 <18633059886@163.com>
2020-06-03 20:58:47 -0400

    Add the revision notes for `lax_reference.py`.

--- tf_jax_stax/lax_reference.py
+++ tf_jax_stax/lax_reference.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# REVISION: Comment out the parts that are related to dtypes.
 
 import builtins
 import collections

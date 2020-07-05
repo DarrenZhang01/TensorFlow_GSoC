bfec084bad09786b9a303776c537b394c1a81427
DarrenZhang01 <18633059886@163.com>
2020-07-04 20:42:35 -0400

    Updated the testing script to use Python 3.8

--- nt_tests.sh
+++ nt_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for f in neural-tangents/neural_tangents/tests/*.py; do 
   rm "${f%.*}.txt"
-  echo "python3.7 $f &> ${f%.*}.txt"
-  python3.7 "$f" &> "${f%.*}.txt" &
+  echo "python3.8 $f &> ${f%.*}.txt"
+  python3.8 "$f" &> "${f%.*}.txt" &
 done

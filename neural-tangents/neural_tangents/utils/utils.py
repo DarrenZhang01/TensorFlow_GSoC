fa9030179345912cb1b3c462152bf0799d725bc5
DarrenZhang01 <18633059886@163.com>
2020-08-02 16:33:53 -0400

    Add `np.array` wrapper to the return value of `choice_a` such that `choice_a` and `choice_b` have the same type of return values

--- neural-tangents/neural_tangents/utils/utils.py
+++ neural-tangents/neural_tangents/utils/utils.py
@@ -395,7 +395,7 @@ def get_masked_array(x: ArrayOrList,
     if mask_constant is None:
       mask = None
     else:
-      choice_a = lambda: tf.math.is_nan(x)
+      choice_a = lambda: np.array(tf.math.is_nan(x))
       choice_b = lambda: x == mask_constant
       # mask = choice_a(x) if math.isnan(mask_constant) else choice_b(x)
       mask = tf.cond(tf.math.is_nan(mask_constant), choice_a, choice_b)

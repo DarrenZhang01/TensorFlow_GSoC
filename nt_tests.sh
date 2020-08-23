b18f2402169bf4658f3eb6b2dd45ca6d07d6e110
DarrenZhang01 <18633059886@163.com>
2020-08-23 13:12:17 -0400

    Update the script to only run the example test files.

--- nt_tests.sh
+++ nt_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for f in neural-tangents/neural_tangents/tests/*.py; do 
-  rm "${f%.*}.txt"
-  echo "python3.8 $f &> ${f%.*}.txt"
-  python3.8 "$f" &> "${f%.*}.txt" &
-done
+#for f in neural-tangents/neural_tangents/tests/*.py; do 
+#  rm "${f%.*}.txt"
+#  echo "python3.8 $f &> ${f%.*}.txt"
+#  python3.8 "$f" &> "${f%.*}.txt" &
+#done
+python3.8 tests/infinite_fcn_test.py &
+python3.8 tests/weight_space_test.py &
+python3.8 tests/function_space_test.py
